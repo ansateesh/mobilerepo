1943bbd5b0c552c79be089afc2ff3dbe86429f5d
Sateesh an <ansateesh@yahoo.com>
2016-04-12 00:00:20 +0530

    commit.

--- MobileProject/src/com/mobile/FileOne.java
+++ MobileProject/src/com/mobile/FileOne.java
@@ -4,13 +4,9 @@ public class FileOne {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// comment 1(r)
-		// comment 2(r)
-		// comment 3(r)
-		// comment 4(r)
-		// comment 5(r)
-		// comment 6(r)
-
+		// new comment
+		// second new comments
+		// third comment4.
 	}
 
 }
